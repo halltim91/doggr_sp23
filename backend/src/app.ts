f157e3a2bdaa49b3591da8b284006e02480655eb
Tim <thall102291@gmail.com>
2023-06-03 21:30:36 -0400

    added missing vite code. set up public npc count request in frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import config from "./db/mikro-orm.config.js";
 import {FastifyMikroOrmPlugin} from "./plugins/mikro.js";
 import {FastifySearchHttpMethodPlugin} from "./plugins/http_search.js";
@@ -6,6 +7,11 @@ import NpcRoutes from "./myRoutes.js";
 
 const app = Fastify();
 
+await app.register(cors, {
+	origin: (origin, cb) => {
+		cb(null, true);
+	}
+});
 await app.register(FastifyMikroOrmPlugin, config);
 await app.register(FastifySearchHttpMethodPlugin);
 await app.register(NpcRoutes);
